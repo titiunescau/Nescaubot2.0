777c126a99b0ea67dcf3517302b92be6fc82968c
titiunescau <81128483+titiunescau@users.noreply.github.com>
2021-04-30 10:40:43 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/titiunescau/Nescaubot2.0/ind.traineddata?raw=true"
 npm install
 
-echo "[*] obrigado por utilizar o bot \"Caussbot\" causs agradece"
+echo "[*] obrigado por utilizar o bot \"Nescauzinbot\" Nescau agradece"
